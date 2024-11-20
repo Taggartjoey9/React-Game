c533326de3b4d1c301188cb6ed36ed84aee10637
Taggartjoey9 <taggartjoey9@gmail.com>
2024-11-19 20:43:03 -0500

    inital gameboard set up - visual

--- tictactoe/src/components/Player.jsx
+++ tictactoe/src/components/Player.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+// eslint-disable-next-line react/prop-types
 export default function Player({ initialName, symbol }) {
 
     const [ playerName, setPlayerName ] = useState(initialName);
